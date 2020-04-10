85a74ed63b81f83b3655ccde292c7ebbd7d3936d
Hugo Kawamata <hugokawamata@gmail.com>
2020-04-10 13:07:38 +1000

    🛁 Remove pods from source control

--- ios/Pods/Target Support Files/Pods-HolisticJapanese-HolisticJapaneseTests/Pods-HolisticJapanese-HolisticJapaneseTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HolisticJapanese_HolisticJapaneseTests : NSObject
-@end
-@implementation PodsDummy_Pods_HolisticJapanese_HolisticJapaneseTests
-@end
--- ios/Pods/Target Support Files/Pods-HolisticJapanese-tvOS/Pods-HolisticJapanese-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HolisticJapanese_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_HolisticJapanese_tvOS
-@end
