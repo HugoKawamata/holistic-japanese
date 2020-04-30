4e1a63424ec31834545d1a048d17a7a1d12bf879
Hugo Kawamata <hugokawamata@gmail.com>
2020-04-30 16:49:14 +1000

    ✨ Use new uri

--- src/apollo.js
+++ src/apollo.js
@@ -2,7 +2,8 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "http://ec2-52-63-127-15.ap-southeast-2.compute.amazonaws.com:4000/",
+  uri:
+    "http://ec2-52-63-127-15.ap-southeast-2.compute.amazonaws.com:4000/graphql",
 });
 
 export default client;
