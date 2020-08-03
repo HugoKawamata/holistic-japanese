90a263183c17ea53238853ca97b2b9a415f3df66
Hugo Kawamata <hugokawamata@gmail.com>
2020-08-03 10:24:17 +1000

    🐛 Remove debugging code

--- src/screens/Learn/index.js
+++ src/screens/Learn/index.js
@@ -236,10 +236,7 @@ export function LearnScreen(props: Props): Node {
 
         // If the me was created less than 5 minutes ago and has no gender,
         // show the introduction
-        if (
-          true ||
-          (me.gender == null && +new Date() - me.createdAt < 60 * 1000 * 5)
-        ) {
+        if (me.gender == null && +new Date() - me.createdAt < 60 * 1000 * 5) {
           props.navigation.push("Introduction", {
             refetch,
           });
